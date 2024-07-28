899faea4b72cd01a66411d2899036cb4cb5ff798
Jay <joaomarcelo1015ja@gmail.com>
2024-07-28 01:48:09 -0300

    fix: bug in tests

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -142,7 +142,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOST =  os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(" ")
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
