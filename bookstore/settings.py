2047272e79221b50c6452bd18580e8665f1f6e8a
Jay <joaomarcelo1015ja@gmail.com>
2024-08-02 22:29:04 -0300

    re-add auth, debug=1, secretkey

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -142,11 +142,11 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-# SECRET_KEY = "p8jv-rc^5=e*%fovbogtqn4l@epi))b3ft-^^6hr+2#&t(#@_h"
+SECRET_KEY = "p8jv-rc^5=e*%fovbogtqn4l@epi))b3ft-^^6hr+2#&t(#@_h"
 
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = int(os.environ.get("DEBUG", default=1))
 
-ALLOWED_HOSTS = ["loaclhost", "127.0.0.1", "http://jayfckz.pythonanywhere.com/", "https://bookstore-production-9091.up.railway.app"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://jayfckz.pythonanywhere.com/", "https://bookstore-production-9091.up.railway.app"]
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
